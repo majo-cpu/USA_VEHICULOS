3cf4d09947c13dc5f8697b431a959d66db1b2dc8
Majo <m.joseathuan@gmail.com>
2024-07-21 21:45:31 -0400

    subir2145

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+st.header('Histograma y Dispersion en funcion de el kilometraje de un vehiculo')
+
+# 
+
+car_data = pd.read_csv('C:/Users/nicol/OneDrive/Documentos/VehiculosUSA/USA_VEHICULOS/vehicles_us.csv',sep=',')  # leer los datos
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir dispersión')
+
+
+## Plots
+if build_histogram:
+    st.write('Histograma de anuncios de venta de coches')
+
+    # Crear histograma
+
+    fig = px.histogram(car_data, x='odometer')
+
+    # Mostrar grafico plotly interactivo
+
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_scatter:
+    st.write( 'Dispersión para los datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x='odometer', y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt upgrade
+pip install --upgrade pip
+pip install --upgrade -r requirements.txt
